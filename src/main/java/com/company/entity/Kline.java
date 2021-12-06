29e49633545e68b367f9c51258a0ea0762fc4034
eskandaridanial <doneskandari@gmail.com>
2021-12-06 15:41:35 +0330

    Add Kline model

---
+++ src/main/java/com/company/entity/Kline.java
@@ -0,0 +1,24 @@
+package com.company.entity;
+
+import lombok.Data;
+
+@Data
+public class Kline {
+
+    private Long t;
+    private Long T;
+    private String s;
+    private String i;
+    private String o;
+    private String c;
+    private String h;
+    private String l;
+    private Boolean x;
+
+}
+
+
+
+
+
+
